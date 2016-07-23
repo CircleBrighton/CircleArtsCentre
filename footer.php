ce63fb0fb92bb162dc9e3aeefe726c4af99bbdcc
Parham Alvani <parham.alvani@gmail.com>
2016-07-24 00:56:11 +0430

    Page footer was completed.

--- footer.php
+++ footer.php
@@ -13,4 +13,23 @@
  *
  */
 ?>
+<hr>
+<footer>
+    <address class="text-center">
+        Circle Arts Centre
+        | Address .... |
+        Registered Charity No:
+    </address>
+    <address class="text-center">
+        <p>
+            <a href="#">Terms &amp; Conditions</a>
+            |
+            <a href="#">Jobs &amp; Opportunities</a>
+        </p>
+        <p>&copy; Circle Arts Centre</p>
+    </address>
+</footer>
+</div>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery-2.2.4.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
 </body>
