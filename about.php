8e78793be677b2dd63ef547e507f1d9fd8078207
Parham Alvani <parham.alvani@gmail.com>
2016-07-24 04:21:44 +0430

    About page and contact page was added.

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * In The Name Of God
+ *
+ * PHP Version 5
+ *
+ * Short description for file
+ *
+ * @category Theme
+ * @package  Wordpress\BehWeb
+ * @author   Parham Alvani <parham.alvani@gmail.com>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ *
+ */
+?>
+<?php
+echo get_post()->post_content;
+?>
