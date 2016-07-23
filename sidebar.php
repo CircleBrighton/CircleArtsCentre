6d9b9f1601fe5d836022794069da5629472e6216
Parham Alvani <parham.alvani@gmail.com>
2016-07-24 00:52:17 +0430

    Sidebar section was added.

---
+++ sidebar.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * In The Name Of God
+ *
+ * PHP Version 5
+ *
+ * Short description for file
+ *
+ * @category Theme
+ * @package  Wordpress\BehWeb
+ * @author   Parham Alvani <parham.alvani@gmail.com>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ *
+ */
+?>
+<div>
+    <ul class="nav nav-pills">
+        <li role="presentation" class="active"><a href="#">About</a></li>
+        <li role="presentation"><a href="#">What's On</a></li>
+        <li role="presentation"><a href="#">Visit</a></li>
+        <li role="presentation"><a href="#">Cafe</a></li>
+        <li role="presentation"><a href="#">Box Office</a></li>
+        <li role="presentation"><a href="#">Support Us</a></li>
+        <li role="presentation"><a href="#">Film Club</a></li>
+        <li role="presentation"><a href="#">Contact</a></li>
+
+        <li role="presentation" class="pull-right">
+            <a href="https://twitter.com/circleartscentre" target="_blank">
+                <img src="<?php bloginfo('template_url'); ?>/img/twitter-logo.jpg" width="28" height="28">
+            </a>
+        </li>
+        <li role="presentation" class="pull-right">
+            <a href="http://www.facebook.com/circleartscentre" target="_blank">
+                <img src="<?php bloginfo('template_url'); ?>/img/facebook-logo.jpg" width="28" height="28">
+            </a>
+        </li>
+    </ul>
+    <div class="well well-sm">
+        Last Tweets
+        <!-- Twitter -->
+    </div>
+</div>
