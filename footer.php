537b6c27b700e2160613e9da66b71a0936a8c536
Parham Alvani <parham.alvani@gmail.com>
2016-07-24 23:41:21 +0430

    Bootstrap and JQuery relation is important :)

--- footer.php
+++ footer.php
@@ -30,8 +30,8 @@
     </address>
 </footer>
 </div>
-<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/jquery-2.2.4.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
