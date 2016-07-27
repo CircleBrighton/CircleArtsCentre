763375b8298f1b2216256cc801f8a83695a99cee
Parham Alvani <parham.alvani@gmail.com>
2016-07-27 23:28:39 +0430

    Event singular page was added.

---
+++ event.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * In The Name Of God
+ *
+ * PHP Version 5
+ *
+ * Short description for file
+ *
+ * @category Theme
+ * @package  Wordpress\BehWeb
+ * @author   Parham Alvani <parham.alvani@gmail.com>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ *
+ */
+?>
+<div class="page-header">
+    <h1><?php echo apply_filters('the_title', get_post()->post_title); ?>
+    <br>
+    <small><?php echo apply_filters('the_date', get_post()->post_date); ?></small></h1>
+</div>
+<table class="table table-bordered">
+    <tr>
+        <td>Date:</td>
+        <td><?php echo get_post_meta(get_post()->ID, 'performed_date', true); ?>
+    </tr>
+    <tr>
+        <td>Time:</td>
+        <td><?php echo get_post_meta(get_post()->ID, 'performed_time', true); ?>
+    </tr>
+    <tr>
+        <td>Price:</td>
+        <td><?php echo get_post_meta(get_post()->ID, 'price', true); ?>
+    </tr>
+</table>
+<?php
+echo apply_filters('the_content', get_post()->post_content);
+?>
