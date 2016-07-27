5a8394dbd5917e6ba7a21c959effd83298c31972
Parham Alvani <parham.alvani@gmail.com>
2016-07-27 16:49:08 +0430

    What's on page was improved.

--- won.php
+++ won.php
@@ -16,16 +16,26 @@
 <?php
 echo apply_filters('the_content', get_post()->post_content);
 ?>
-<?php $q = new WP_Query(array ('post_type' => 'circle_event')); ?>
-<div class="list-group">
-<?php while($q->have_posts()): $q->the_post(); $i = 1; ?>
-    <a class="list-group-item" href="<?php the_permalink(); ?>">
-        <h4 class="list-group-item-heading"><?php the_title(); ?></h4>
-        <p class="list-group-item-text">
-            <ul>
-                <li>Performed Date: <?php echo get_post_meta(get_the_ID(), 'performed_date', true); ?></li>
-            </ul>
-        </p>
-    </a>
+<?php $q = new WP_Query(array ('post_type' => 'circle_event')); $i = 0; ?>
+<table class="table table-stripedi table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Date</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Time</th>
+        </tr>
+    </thead>
+    <tbody>
+<?php while($q->have_posts()): $q->the_post(); $i++; ?>
+        <tr data-href="<?php the_permalink(); ?>">
+            <td><?php echo $i ?></td>
+            <td><?php echo get_post_meta(get_the_ID(), 'performed_date', true); ?>
+            <td><?php the_title(); ?></td>
+            <td><?php echo get_post_meta(get_the_ID(), 'performed_time', true); ?>
+            <td><?php echo get_post_meta(get_the_ID(), 'price', true); ?>
+        </tr>
 <?php endwhile; ?>
-</div>
+    </tbody>
+</table>
