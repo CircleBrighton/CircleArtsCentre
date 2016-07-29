665cd36ccc58f47cf9e6c29d2a75db0730314903
Parham Alvani <parham.alvani@gmail.com>
2016-07-29 21:47:59 +0430

    Functions file was reformatted.

--- functions.php
+++ functions.php
@@ -23,7 +23,8 @@ add_theme_support('post-thumbnails');
  * @param int $length Excerpt length.
  * @return int (Maybe) modified excerpt length.
  */
-function wpdocs_custom_excerpt_length($length) {
+function wpdocs_custom_excerpt_length($length)
+{
     return 10;
 }
 add_filter('excerpt_length', 'wpdocs_custom_excerpt_length');
@@ -34,7 +35,8 @@ add_filter('excerpt_length', 'wpdocs_custom_excerpt_length');
  * @param string $more "Read more" excerpt string.
  * @return string (Maybe) modified "read more" excerpt string.
  */
-function wpdocs_excerpt_more($more) {
+function wpdocs_excerpt_more($more)
+{
     /* uncomment following code in order to have button for read more. */
     /*
     return sprintf('<div><a class="btn btn-primary" href="%1$s">%2$s</a></div>',
@@ -50,8 +52,10 @@ add_filter('excerpt_more', 'wpdocs_excerpt_more');
  * Register new post type(s).
  */
 add_action('init', 'create_post_type');
-function create_post_type() {
-    register_post_type('circle_event',
+function create_post_type()
+{
+    register_post_type(
+        'circle_event',
         array(
             'labels' => array(
                 'name' => __('Events'),
@@ -69,25 +73,29 @@ function create_post_type() {
 /**
  * Register meta box(es).
  */
-function wpdocs_register_meta_boxes() {
-    add_meta_box('event-info', __('Event Info', 'textdomain'), 'wpdocs_display_event_callback', 'circle_event' );
+function wpdocs_register_meta_boxes()
+{
+    add_meta_box('event-info', __('Event Info', 'textdomain'), 'wpdocs_display_event_callback', 'circle_event');
 }
 add_action('add_meta_boxes', 'wpdocs_register_meta_boxes');
 
-function wpdocs_save_meta($post_id, $post) {
+function wpdocs_save_meta($post_id, $post)
+{
     /* Verify the nonce before proceeding. */
-    if ( !isset( $_POST['circle_event_nonce'] ) || !wp_verify_nonce( $_POST['circle_event_nonce'], basename( __FILE__ )))
+    if (!isset($_POST['circle_event_nonce']) || !wp_verify_nonce($_POST['circle_event_nonce'], basename(__FILE__))) {
         return $post_id;
+    }
 
     /* Get the post type object. */
     $post_type = get_post_type_object($post->post_type);
 
     /* Check if the current user has permission to edit the post. */
-    if (!current_user_can($post_type->cap->edit_post, $post_id))
+    if (!current_user_can($post_type->cap->edit_post, $post_id)) {
         return $post_id;
+    }
 
     /* Get the posted data. */
-    $new_meta_value = (isset( $_POST['circle-event-performed-date'] ) ? $_POST['circle-event-performed-date'] : '');
+    $new_meta_value = (isset($_POST['circle-event-performed-date']) ? $_POST['circle-event-performed-date'] : '');
 
     /* Get the meta key. */
     $meta_key = 'performed_date';
@@ -96,19 +104,18 @@ function wpdocs_save_meta($post_id, $post) {
     $meta_value = get_post_meta($post_id, $meta_key, true);
 
     /* If a new meta value was added and there was no previous value, add it. */
-    if ($new_meta_value && '' == $meta_value)
+    if ($new_meta_value && '' == $meta_value) {
         add_post_meta($post_id, $meta_key, $new_meta_value, true);
-
-    /* If the new meta value does not match the old value, update it. */
-    elseif ($new_meta_value && $new_meta_value != $meta_value)
+    } /* If the new meta value does not match the old value, update it. */
+    elseif ($new_meta_value && $new_meta_value != $meta_value) {
         update_post_meta($post_id, $meta_key, $new_meta_value);
-
-    /* If there is no new meta value but an old value exists, delete it. */
-    elseif ('' == $new_meta_value && $meta_value)
+    } /* If there is no new meta value but an old value exists, delete it. */
+    elseif ('' == $new_meta_value && $meta_value) {
         delete_post_meta($post_id, $meta_key, $meta_value);
+    }
 
     /* Get the posted data. */
-    $new_meta_value = (isset( $_POST['circle-event-performed-time'] ) ? $_POST['circle-event-performed-time'] : '');
+    $new_meta_value = (isset($_POST['circle-event-performed-time']) ? $_POST['circle-event-performed-time'] : '');
 
     /* Get the meta key. */
     $meta_key = 'performed_time';
@@ -117,19 +124,18 @@ function wpdocs_save_meta($post_id, $post) {
     $meta_value = get_post_meta($post_id, $meta_key, true);
 
     /* If a new meta value was added and there was no previous value, add it. */
-    if ($new_meta_value && '' == $meta_value)
+    if ($new_meta_value && '' == $meta_value) {
         add_post_meta($post_id, $meta_key, $new_meta_value, true);
-
-    /* If the new meta value does not match the old value, update it. */
-    elseif ($new_meta_value && $new_meta_value != $meta_value)
+    } /* If the new meta value does not match the old value, update it. */
+    elseif ($new_meta_value && $new_meta_value != $meta_value) {
         update_post_meta($post_id, $meta_key, $new_meta_value);
-
-    /* If there is no new meta value but an old value exists, delete it. */
-    elseif ('' == $new_meta_value && $meta_value)
+    } /* If there is no new meta value but an old value exists, delete it. */
+    elseif ('' == $new_meta_value && $meta_value) {
         delete_post_meta($post_id, $meta_key, $meta_value);
+    }
 
     /* Get the posted data. */
-    $new_meta_value = (isset( $_POST['circle-event-price'] ) ? $_POST['circle-event-price'] : '');
+    $new_meta_value = (isset($_POST['circle-event-price']) ? $_POST['circle-event-price'] : '');
 
     /* Get the meta key. */
     $meta_key = 'price';
@@ -138,16 +144,15 @@ function wpdocs_save_meta($post_id, $post) {
     $meta_value = get_post_meta($post_id, $meta_key, true);
 
     /* If a new meta value was added and there was no previous value, add it. */
-    if ($new_meta_value && '' == $meta_value)
+    if ($new_meta_value && '' == $meta_value) {
         add_post_meta($post_id, $meta_key, $new_meta_value, true);
-
-    /* If the new meta value does not match the old value, update it. */
-    elseif ($new_meta_value && $new_meta_value != $meta_value)
+    } /* If the new meta value does not match the old value, update it. */
+    elseif ($new_meta_value && $new_meta_value != $meta_value) {
         update_post_meta($post_id, $meta_key, $new_meta_value);
-
-    /* If there is no new meta value but an old value exists, delete it. */
-    elseif ('' == $new_meta_value && $meta_value)
+    } /* If there is no new meta value but an old value exists, delete it. */
+    elseif ('' == $new_meta_value && $meta_value) {
         delete_post_meta($post_id, $meta_key, $meta_value);
+    }
 }
 add_action('save_post', 'wpdocs_save_meta', 10, 2);
 
@@ -156,8 +161,9 @@ add_action('save_post', 'wpdocs_save_meta', 10, 2);
  *
  * @param WP_Post $post Current post object.
  */
-function wpdocs_display_event_callback($post) {
-    wp_nonce_field(basename( __FILE__ ), 'circle_event_nonce');
+function wpdocs_display_event_callback($post)
+{
+    wp_nonce_field(basename(__FILE__), 'circle_event_nonce');
 ?>
   <div>
     <label for="circle-event-performed-date">Performed Date: </label>
@@ -177,17 +183,18 @@ function wpdocs_display_event_callback($post) {
 <?php
 }
 
-function circle_customize_register($wp_customize) {
-    $wp_customize->add_setting('aside_row_title' , array(
+function circle_customize_register($wp_customize)
+{
+    $wp_customize->add_setting('aside_row_title', array(
         'default'     => 'Volunteering',
         'transport'   => 'refresh',
     ));
-    $wp_customize->add_section('aside_rows' , array(
-        'title'      => __( 'Aside Rows', 'circle' ),
+    $wp_customize->add_section('aside_rows', array(
+        'title'      => __('Aside Rows', 'circle'),
         'priority'   => 30,
     ));
     $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'link_color', array(
-        'label'        => __( 'Aside Row Title', 'circle' ),
+        'label'        => __('Aside Row Title', 'circle'),
         'section'    => 'aside_rows',
         'settings'   => 'aside_row_title',
     )));
