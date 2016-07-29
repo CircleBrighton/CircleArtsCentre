df7f8374d0d2a31d67daf9dd89f602601feb078a
Parham Alvani <parham.alvani@gmail.com>
2016-07-30 00:23:11 +0430

    General page template was added.

---
+++ general.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * In The Name Of God
+ *
+ * PHP Version 5
+ *
+ * Short description for file
+ *
+ * @category Theme
+ * @package  Wordpress\BehWeb
+ * @author   Parham Alvani <parham.alvani@gmail.com>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ *
+ */
+?>
+<?php
+echo apply_filters('the_content', get_post()->post_content);
