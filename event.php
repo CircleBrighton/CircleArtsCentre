a08fed357b1e4b7f3921635a9f5dc73f9046003a
Parham Alvani <parham.alvani@gmail.com>
2016-07-31 10:34:18 +0430

    Event page was improved.

--- event.php
+++ event.php
@@ -17,7 +17,8 @@
     <h1><?php echo apply_filters('the_title', get_post()->post_title); ?>
     <br>
     <small><?php echo apply_filters('the_date', get_post()->post_date); ?></small>
-    <a class="btn btn-primary pull-right" href="<?php echo esc_attr(get_option('buy_ticket_link', '')) ?>">
+    <a class="btn btn-primary pull-right" 
+        href="<?php echo esc_attr(get_option('buy_ticket_link', '')).'/'.get_post()->post_name; ?>">
         Buy Ticket
     </a>
     </h1>
