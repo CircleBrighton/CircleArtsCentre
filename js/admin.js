6b5fa5ed81e496a9b7ad0a357866a4fb57e7d67b
Parham Alvani <parham.alvani@gmail.com>
2016-08-01 04:58:20 +0430

    Admin page javascript was added.

---
+++ js/admin.js
@@ -0,0 +1,13 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 27-07-2016
+ * |
+ * | File Name:     admin.js
+ * +===============================================
+ */
+webshims.setOptions('waitReady', false)
+webshims.setOptions('forms-ext', {types: 'date'})
+webshims.polyfill('forms forms-ext')
+
