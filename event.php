82fc12e83bd24743e3fc8ee31f340abb594b73ff
Parham Alvani <parham.alvani@gmail.com>
2016-08-04 11:55:58 +0430

    Show event button based on it's status.

--- event.php
+++ event.php
@@ -12,14 +12,36 @@
  * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
  *
  */
+$status = get_post_meta(get_post()->ID, 'status', true);
 ?>
 <div class="page-header">
     <h1><?php echo apply_filters('the_title', get_post()->post_title); ?>
     <br>
     <small><?php echo apply_filters('the_date', get_post()->post_date); ?></small>
-    <a class="btn btn-primary pull-right"
-        href="<?php echo esc_attr(get_post_meta(get_post()->ID, 'buy_link', true)); ?>">
-        Buy Ticket
+    <a class="btn btn-primary pull-right
+        <?php $status == '1' || $status == '2' ? 'disabled' : '' ?>"
+        href="<?php $status == '0' ? esc_attr(get_post_meta(get_post()->ID, 'buy_link', true)) : '#' ?>">
+<?php
+switch ($status) {
+    case '0':
+        echo "Buy Ticket";
+        break;
+    case '1':
+        echo "Finished";
+        break;
+    case '2':
+        echo "Cancelled";
+        break;
+    case '3':
+        echo "Free Entry";
+        break;
+    case '4':
+        echo "Members Only";
+        break;
+    default:
+        echo "Buy Ticket";
+}
+?>
     </a>
     </h1>
 </div>
