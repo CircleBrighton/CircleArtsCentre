ba8a937dab4a725acc0066f1cc0ce268a43be3a9
Parham Alvani <parham.alvani@gmail.com>
2016-08-19 12:48:27 +0430

    Nothing :)

--- declaration.php
+++ declaration.php
@@ -9,7 +9,7 @@
  *
  * @category CircleTheme
  * @file declaration.php
- * @package  \
+ * @package
  * @author   Parham Alvani <parham.alvani@gmail.com>
  * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
  *
