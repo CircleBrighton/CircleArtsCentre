0f7ee5130c5526dd522a6952d221635cebad9e68
Parham Alvani <parham.alvani@gmail.com>
2016-08-24 00:50:28 +0430

    Archive page was completed :)

--- archive.php
+++ archive.php
@@ -58,6 +58,49 @@
 <hr>
 <div>
     <div class="row">
-    </div>
+<?php for ($i = 0; $i < (int)get_theme_mod('feature_box_number', '3'); $i++) : ?>
+<?php
+$q = new WP_Query(array ('category_name' => 'Feature Box '.$i, 'post_type' => array('post', 'circle_event')));
+if ($q->have_posts()) :
+    $q->the_post();
+?>
+<?php
+switch (get_theme_mod('feature_box_number', '3')) {
+    case '1':
+        echo '<div class="col-md-3 col-md-offset-4">';
+        break;
+    case '2':
+        echo '<div class="col-md-3 col-md-offset-2">';
+        break;
+    case '3':
+        echo '<div class="col-md-3 col-md-offset-1 col-xs-4">';
+        break;
+    case '4':
+        echo '<div class="col-md-3">';
+        break;
+}
+?>
+            <div class="thumbnail text-justify">
+                <div class="thumbnail bg-primary">
+                <h3 class="fg-white text-center">
+                    <?php echo get_theme_mod('feature_box_'.$i.'_title', 'Title'); ?>
+                </h3>
+<?php
+if (has_post_thumbnail()) {
+    the_post_thumbnail();
+}
+?>
+                </div>
+                <h4><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                    <?php the_title(); ?>
+                </a></h4>
+                <?php wp_trim_words(the_excerpt(), 55); ?>
+                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                    Read More
+                </a>
+            </div>
+        </div>
+<?php endif; ?>
+<?php endfor; ?>
 </div>
 <?php get_footer(); ?>
