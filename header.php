0a329d70bb65d006268585a2628d24d282075c9a
Parham Alvani <parham.alvani@gmail.com>
2016-09-23 12:25:38 +0330

    Page header issue was corrected.

--- header.php
+++ header.php
@@ -17,7 +17,10 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
-    <title><?php wp_title(); ?></title>
+    <title>
+        <?php echo get_bloginfo('name'); ?>
+        <?php wp_title(); ?>
+    </title>
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
